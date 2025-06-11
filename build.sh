0eb74e6a730746f02c86f405e79449dfb8e48be6
geomanuk20 <geomanuk20@gmail.com>
2025-06-11 12:05:08 +0530

    first

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+# Exit on any error
 set -o errexit
+
+# Install Python dependencies
 pip install -r requirements.txt
+
+# Run database migrations (if using Django)
 python manage.py migrate
+
+# (Optional) Collect static files (if using Django)
 python manage.py collectstatic --noinput
